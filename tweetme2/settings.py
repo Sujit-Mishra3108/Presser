a3949c01b56f79101087be000dc4eaee62d7e18f
Sujit-Mishra3108 <Sujitmishra77797@gmail.com>
2021-01-23 00:17:26 +0530

    templates

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# remember on same level as manage.py
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tweetme2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+
+from .models import Tweet
 # Create your views here.
+
 def home_view(request, *ags,**kwargs):
-    return HttpResponse("<h2>Hello World</h2>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_detail_view(request,tweet_id, *args,**kwargs):
+
+    '''
+    REST API VIEW 
+    consume by JavaScript or Swift/JAVA/iOS/Andriod
+    return json data
+    '''
+    data = {
+        "id" : tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+    
+    return JsonResponse(data, status=status) #json.dumps content_type = 'application/json'
